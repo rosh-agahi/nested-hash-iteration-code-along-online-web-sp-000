533476d1e25d8e9617a54bf9a99283448c9f11c5
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-06 19:47:03 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,6 @@ require 'pry'
   
 def remove_strawberry(contacts)
 
-contacts["Freddy Mercury"][:favorite_ice_cream_flavors].reject {|k,v| 
+contacts["Freddy Mercury"][:favorite_ice_cream_flavors].reject {|k,v| v == "strawberry"}
 
 end
